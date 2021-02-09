e18b84fec5f28eb44756b1debada635099d72033
seunghw <ysp1307@naver.com>
2021-02-09 20:06:24 +0900

    0209

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser"); // clientserver에서 분석해서 가져올 수 있게해주는것.
-const { User } = require("./models/User");
+
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+const { User } = require("./models/User");
+const { auth } = require("./middleware/auth");
 
 // application/x-www-form-ulencoded << 이렇게 된 데이터를 분석해서 가져올 수 있게해줌
 app.use(bodyParser.urlencoded({ extended: true }));
 // application/json << 이렇게 된 데이터를 분석해서 가져올 수 있게 해 줌
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -25,7 +29,7 @@ app.get("/", (req, res) => {
   res.send("안녕하세요!");
 });
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   // 회원 가입할 때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.
   console.log(req);
@@ -36,6 +40,65 @@ app.post("/register", (req, res) => {
     //
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.post("/api/users/login", (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 있는지 찾는다
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인.
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+
+      // 비밀번호까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. 어디에? 쿠키 , 로컬스토리지 장단점이 있음
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
+  });
+});
+
+// role 1 어디믄 role 2 특정 부서 어드민
+// role 0 -> 일반유저   role이 0이 아니면 관리자
+app.get("/api/users/auth", auth, (req, res) => {
+  //auth란? callback하기 전에 중간 처리
+
+  //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True 라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
       success: true,
     });
   });
